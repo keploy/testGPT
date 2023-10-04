f9faf5a7cf750616084a2b34a37f1cafb2e0cbe7
Animesh <kurosakiichigo.songoku@gmail.com>
2023-10-04 15:28:57 +0530

    feat: shell script ready
    Signed-off-by: Animesh <kurosakiichigo.songoku@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM golang:1.20 AS bookworm
+
+WORKDIR /app
+
+# === Runtime Stage ===
+FROM ubuntu:latest
+
+# Install curl and sudo
+RUN apt-get update && \
+    apt-get install -y curl sudo && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN curl -fsSL https://get.docker.com -o get-docker.sh && \
+    sh get-docker.sh && \
+    rm get-docker.sh
+
+
+ENTRYPOINT [ "install.sh" ]
---
+++ install.sh
@@ -0,0 +1,25 @@
+# Path: install.sh
+if ! [ -e .git ]; then
+    echo "Please run this from where main.go file is present"
+    exit 1
+fi
+
+# Install Keploy binary using curl command
+curl --silent --location "https://github.com/keploy/keploy/releases/latest/download/keploy_linux_amd64.tar.gz" | tar xz -C /tmp
+echo curl --silent --location "https://github.com/keploy/keploy/releases/latest/download/keploy_linux_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv /tmp/keploy /usr/local/bin/keploy
+echo sudo mv /tmp/keploy /usr/local/bin/keploy
+
+chmod +x /usr/local/bin/keploy
+
+echo "Keploy installed successfully 🎉"
+
+# Generate app binary
+go mod download
+echo 'mod download'
+go build -o application
+echo 'go build -o application .'
+
+sudo -E env PATH=$PATH keploy test -c "${GITHUB_WORKSPACE}/application" --delay ${DELAY} --debug
+echo sudo -E env PATH=$PATH keploy test -c "${GITHUB_WORKSPACE}/application" --delay ${DELAY}
