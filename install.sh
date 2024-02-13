64ecf666a8f59d66af8cbd48febec59c017543be
Yaxhveer <yaxhcod@gmail.com>
2024-02-13 19:05:33 +0530

    updated changes
    Signed-off-by: Yaxhveer <yaxhcod@gmail.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Install Keploy binary using curl command
 curl --silent --location "https://github.com/keploy/keploy/releases/latest/download/keploy_linux_amd64.tar.gz" | tar xz -C /tmp
-echo "curl --silent --location "https://github.com/keploy/keploy/releases/latest/download/keploy_linux_amd64.tar.gz" | tar xz -C /tmp"
+echo "curl --silent --location 'https://github.com/keploy/keploy/releases/latest/download/keploy_linux_amd64.tar.gz' | tar xz -C /tmp"
 
 sudo mv /tmp/keploy /usr/local/bin/keploy
 chmod +x /usr/local/bin/keploy
